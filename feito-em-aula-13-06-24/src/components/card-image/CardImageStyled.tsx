ca78669d786b7f4c84306d08421476009ed91189
Joel Luis Steffen <jo.lsteffen@gmail.com>
2024-06-17 10:53:29 -0300

    first commit

---
+++ feito-em-aula-13-06-24/src/components/card-image/CardImageStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+const CardImageStyled = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  height: 100%;
+  justify-content: center;
+`;
+
+export default CardImageStyled;
